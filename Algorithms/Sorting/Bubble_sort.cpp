aa06b2e9dec567bfd260cbff46798735f39d65c5
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-06-12 11:38:55 +0530

    Update Bubble_sort.cpp

--- Algorithms/Sorting/Bubble_sort.cpp
+++ Algorithms/Sorting/Bubble_sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void bubble_sort(int a[],int n){
   for (int i=0;i<n;i++){
     for (int j=0;j<n-i-1;j++){
-      if(a[j]>a[j+1]){
+      if(a[j]>a[j+1]){ //compare 
         swap(a[j],a[j+1]);
       }
     }
