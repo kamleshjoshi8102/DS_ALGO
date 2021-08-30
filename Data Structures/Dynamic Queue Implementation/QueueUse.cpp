ec769f127c8b5906f20b445f286af26186f698b3
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-08-30 15:55:04 +0530

    Create QueueUse.cpp

---
+++ Data Structures/Dynamic Queue Implementation/QueueUse.cpp
@@ -0,0 +1,26 @@
+//dynamic  queue implementation
+#include <iostream>
+#include "Queue.cpp"
+using namespace std;
+
+int main(){
+    Queue<int> q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.enqueue(60); 
+    q.enqueue(70); 
+    
+    cout<<q.front()<<endl;  //10
+    cout<<q.dequeue()<<endl; //10
+    cout<<q.dequeue()<<endl;  //20
+    cout<<q.dequeue()<<endl; //30
+    
+    cout<<q.front()<<endl; //40
+    cout<<q.getSize()<<endl; //2
+    cout<<q.isEmpty()<<endl; //0
+}
+
+
